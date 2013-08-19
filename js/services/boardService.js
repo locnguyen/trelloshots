3bc840c677f9c52665f8e8e55f8fa5bb74039fed
Loc Nguyen <lochnguyen@gmail.com>
2013-08-19 16:26:50 -0700

    Removing BoardConfig function that was extracted into configService

--- js/services/boardService.js
+++ js/services/boardService.js
@@ -22,45 +22,4 @@
       }
     }
   }]);
-
-  function BoardConfig(board) {
-    var selectedListIds = [],
-        selectedColumns = [];
-
-    this.id = board.id;
-
-    this.addSelectedList = function(listId) {
-      selectedListIds.push(listId)
-      selectedListIds = _.uniq(selectedListIds);
-    }
-
-    this.removeSelectedList = function(listId) {
-      selectedListIds = _.filter(selectedListIds, function(id) { return listId !== id; });
-    }
-
-    this.isSelectedList = function(listId) {
-      return _.contains(selectedListIds, listId);
-    }
-
-    this.selectedListIds = function() {
-      return selectedListIds;
-    }
-
-    this.addSelectedColumn = function(column) {
-      selectedColumns.push(column);
-      selectedColumns = _.compact(_.uniq(selectedColumns));
-    }
-
-    this.removeSelectedColumn = function(column) {
-      selectedColumns = _.filter(selectedColumns, function(c) { return c !== column; });
-    }
-
-    this.isSelectedColumn = function(column) {
-      return _.contains(selectedColumns, column);
-    }
-
-    this.selectedColumns = function() {
-      return selectedColumns;
-    }
-  }
 }());
