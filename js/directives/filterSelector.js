64cd0d458cea597894f0eefe641ae9e225ed423d
Loc Nguyen <lochnguyen@gmail.com>
2013-08-20 15:39:03 -0700

    Initial commit for filters modal

---
+++ js/directives/filterSelector.js
@@ -0,0 +1,11 @@
+(function() {
+  TrelloShots.app.directive('filterSelector', [function() {
+    return {
+      templateUrl: '/partials/filterSelector.html',
+      restrict: 'EA',
+      link: function(scope, el, attrs) {
+
+      }
+    }
+  }]);
+}());
