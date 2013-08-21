8485235978089e152992ecf247d5bbeec29b6bee
Loc Nguyen <lochnguyen@gmail.com>
2013-08-20 22:47:53 -0700

    Clearing previous board config url params whenever another board is in view

--- js/controllers/BoardDetailController.js
+++ js/controllers/BoardDetailController.js
@@ -1,63 +1,72 @@
-(function() {
-  TrelloShots.app.controller('BoardDetailController', [
-      '$scope', '$location', '$routeParams', 'boardService', 'viewStateService', 'listService', 'configService',
-    function($scope, $location, $routeParams, boardService, viewStateService, listService, configService) {
+(function () {
+    TrelloShots.app.controller('BoardDetailController', [
+        '$scope', '$location', '$routeParams', 'boardService', 'viewStateService', 'listService', 'configService',
+        function ($scope, $location, $routeParams, boardService, viewStateService, listService, configService) {
 
-      boardService.get($routeParams.boardId).then(function(board) {
-        viewStateService.currentBoard = board;
-        $scope.boardConfig = configService.board(board.id);
-        $location.search('selectedListIds', $scope.boardConfig.selectedListIds);
-        $location.search('selectedColumns', $scope.boardConfig.selectedColumns);
-      });
+            $location.search('selectedListIds', null);
+            $location.search('selectedColumns', null);
 
-      $scope.$watch(
-        function() { return $scope.boardConfig && _.size($scope.boardConfig.selectedListIds); },
-        function() {
-          if (!$scope.boardConfig) {
-            $scope.cards = [];
-          }
-          else {
-            $scope.selectedListIds = $scope.boardConfig.selectedListIds;
-            listService.cards($scope.selectedListIds).then(function(cards) {
-              $scope.cards = cards;
+            boardService.get($routeParams.boardId).then(function (board) {
+                viewStateService.currentBoard = board;
+                $scope.boardConfig = configService.board(board.id);
+                $location.search('selectedListIds', $scope.boardConfig.selectedListIds);
+                $location.search('selectedColumns', $scope.boardConfig.selectedColumns);
             });
-          }
-        }
-      );
 
-      $scope.$watch(
-        function() { return $scope.boardConfig && _.size($scope.boardConfig.selectedColumns); },
-        function() {
-          if ($scope.boardConfig) {
-            $scope.selectedColumns = $scope.boardConfig.selectedColumns;
-            $scope.columnDefinitions = _.filter(listService.cardColumnDefinitions(), function(c) {
-              return _.contains($scope.selectedColumns, c.field);
-            });
-          }
-          else {
-            $scope.columnDefinitions = [];
-          }
-        }
-      );
+            $scope.$watch(
+                function () {
+                    return $scope.boardConfig && _.size($scope.boardConfig.selectedListIds);
+                },
+                function () {
+                    if (!$scope.boardConfig) {
+                        $scope.cards = [];
+                    }
+                    else {
+                        $scope.selectedListIds = $scope.boardConfig.selectedListIds;
+                        listService.cards($scope.selectedListIds).then(function (cards) {
+                            $scope.cards = cards;
+                        });
+                    }
+                }
+            );
 
-      $scope.gridOptions = { data: 'cards', columnDefs: 'columnDefinitions' };
+            $scope.$watch(
+                function () {
+                    return $scope.boardConfig && _.size($scope.boardConfig.selectedColumns);
+                },
+                function () {
+                    if ($scope.boardConfig) {
+                        $scope.selectedColumns = $scope.boardConfig.selectedColumns;
+                        $scope.columnDefinitions = _.filter(listService.cardColumnDefinitions(), function (c) {
+                            return _.contains($scope.selectedColumns, c.field);
+                        });
+                    }
+                    else {
+                        $scope.columnDefinitions = [];
+                    }
+                }
+            );
 
-      $scope.member = function(idOrUsername) {
-        return _.find($scope.currentBoard.members, function(m) {
-          return m.id === idOrUsername || m.username === idOrUsername;
-        })
-      }
+            $scope.gridOptions = { data: 'cards', columnDefs: 'columnDefinitions' };
 
-      $scope.pluckCardUsernames = function(idsOrUsernames) {
-        var members = _.map(idsOrUsernames, function(id) {
-           return $scope.member(id);
-        });
+            $scope.member = function (idOrUsername) {
+                return _.find($scope.currentBoard.members, function (m) {
+                    return m.id === idOrUsername || m.username === idOrUsername;
+                })
+            }
 
-        return _.pluck(members, 'username').join(', ');
-      }
+            $scope.pluckCardUsernames = function (idsOrUsernames) {
+                var members = _.map(idsOrUsernames, function (id) {
+                    return $scope.member(id);
+                });
 
-      $scope.pluckCardListName = function(id) {
-         return _.find($scope.currentBoard.lists, function(l) { return l.id === id; }).name;
-      }
-  }]);
+                return _.pluck(members, 'username').join(', ');
+            }
+
+            $scope.pluckCardListName = function (id) {
+                return _.find($scope.currentBoard.lists,function (l) {
+                    return l.id === id;
+                }).name;
+            }
+        }]);
 }());
